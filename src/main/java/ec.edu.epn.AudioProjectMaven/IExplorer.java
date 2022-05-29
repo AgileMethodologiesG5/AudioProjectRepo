68d8d33f84d95d96d4f061a52648b41707965cb9
Andrés Lozano <andreslozanoestrada@outlook.com>
2022-05-28 23:05:45 -0500

    MOCK UNIT TEST 2 - asserNotNull: a mock unit test was made to simulate the selection of an audio file made with the setAudioFileByName method.

--- src/main/java/ec.edu.epn.AudioProjectMaven/IExplorer.java
+++ src/main/java/ec.edu.epn.AudioProjectMaven/IExplorer.java
@@ -1,7 +1,11 @@
 package ec.edu.epn.AudioProjectMaven;
 
+import java.io.File;
+
 public interface IExplorer {
 
     public boolean deleteFileByName(String fileName);
+
+    public File setAudioFileByName(String fileName);
 
 }
--- src/test/java/ec.edu.epn.AudioProjectMaven/IExplorerTest.java
+++ src/test/java/ec.edu.epn.AudioProjectMaven/IExplorerTest.java
@@ -3,6 +3,8 @@ package ec.edu.epn.AudioProjectMaven;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.io.File;
+
 import static org.junit.Assert.*;
 
 public class IExplorerTest {
@@ -13,6 +15,16 @@ public class IExplorerTest {
         Mockito.when(e.deleteFileByName("cheer.wav")).thenReturn(true);
 
         assertEquals(true, e.deleteFileByName("cheer.wav"));
+    }
+
+    @Test
+    public void given_a_file_name_string_when_selecting_file_then_ok() {
+        IExplorer e = Mockito.mock(IExplorer.class);
+        Mockito.when(e.setAudioFileByName("cheer.wav")).thenReturn(
+                new File("C:\\Users\\Confectus\\Downloads\\AudioProjectRepo\\audioLibrary\\cheer.wav")
+        );
+
+        assertNotNull(e.setAudioFileByName("cheer.wav"));
     }
 
 }
