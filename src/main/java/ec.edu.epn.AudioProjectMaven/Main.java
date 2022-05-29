eabf75b615390d29f09d726714f56faf9e12940b
Francisco Encalada <francisco.encalada01@epn.edu.ec>
2022-05-29 00:13:03 -0500

    REFACTOR 2 - Substitute Algorithm: The main method was modified to be clearer and execute the CLI class efficiently.

--- src/main/java/ec.edu.epn.AudioProjectMaven/Main.java
+++ src/main/java/ec.edu.epn.AudioProjectMaven/Main.java
@@ -1,42 +1,14 @@
 package ec.edu.epn.AudioProjectMaven;
 
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
-        Explorer explorer = new Explorer();
+    public static void main(String[] args) throws IOException {
 
-        // Creating an explorer and listing all the files in the targeted directory
-        String filesList = explorer.getFilesList();
-        System.out.println(filesList);
+        String OUT_PATH = System.getProperty("user.dir") + "\\out\\production";
+        ProcessBuilder pb = new ProcessBuilder("cmd","/c start cmd.exe /K","cd","src","&&","javac","-d",OUT_PATH,"Cli.java","&&","java","-cp",OUT_PATH,"Cli.java");
+        pb.start();
 
-        // Selecting and playing a specific audio file
-        explorer.setAudioFileByName("arc1.wav");
-        explorer.playAudioFile();
-
-        // Converting the targeted audio file to other audio formats
-        {
-            explorer.convertAudioFile(Format.WAV);
-            explorer.convertAudioFile(Format.AU);
-            explorer.convertAudioFile(Format.AIFF);
-
-            explorer.setAudioFileByName("arc1.aiff");
-            explorer.convertAudioFile(Format.WAV);
-            explorer.convertAudioFile(Format.AU);
-            explorer.convertAudioFile(Format.AIFF);
-
-            explorer.setAudioFileByName("arc1.au");
-            explorer.convertAudioFile(Format.WAV);
-            explorer.convertAudioFile(Format.AU);
-            explorer.convertAudioFile(Format.AIFF);
-        }
-
-        //Testing move files between directories
-        String initialPath = System.getProperty("user.dir") + "\\audioLibrary\\converterOut\\WAV";
-        String finalPath = System.getProperty("user.dir") + "\\audioLibrary\\converterOut";
-        explorer.moveFile("arc1.wav", initialPath, finalPath);
     }
 }
 
